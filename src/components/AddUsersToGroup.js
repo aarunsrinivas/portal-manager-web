15b25741b9003f0a5e52540808802dbdcce63fad
Aarun Srinivas <asrinivas35@gatech.edu>
2021-11-30 21:35:59 -0500

    first commit

---
+++ src/components/AddUsersToGroup.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { groups, setAllGroups } from './state/GroupState'
+import { userList } from './state/UserState'
+import endpoints from '../enpoints'
+import '../component-styles/AddUsersToGroup.css'
+
+const AddUsersToGroup = () => {
+  const [selectedGroup, setSelectedGroup] = useState(0)
+  const [checkBoxes, setCheckBoxes] = useState([])
+
+  const allGroups = groups.use()
+  const allUsers = userList.use()
+
+  const handleUsersOnChange = (index) => {
+    let tempCheckBoxes = [...checkBoxes]
+    tempCheckBoxes[index] = !tempCheckBoxes[index]
+    setCheckBoxes(tempCheckBoxes)
+  }
+
+  const handleGroupsOnChange = (index) => {
+    setSelectedGroup(index)
+  }
+
+  const getUsersAndGroups = () => {
+    const newUsers = allUsers.filter((element, index) => {
+      return checkBoxes[index] === true
+    })
+    const groupToAddTo = allGroups[selectedGroup]
+
+    return { newUsers, groupToAddTo }
+  }
+
+  const removeUsersFromGroup = () => {
+    const { newUsers, groupToAddTo } = getUsersAndGroups()
+
+    console.log('Remove Group ' + groupToAddTo.name + ' ' + groupToAddTo.id)
+    newUsers.forEach((e) => console.log(e.email + ' ' + e.id))
+  }
+
+  const assignUsersToGroup = () => {
+    const { newUsers, groupToAddTo } = getUsersAndGroups()
+
+    console.log('Add Group ' + groupToAddTo.name + ' ' + groupToAddTo.id)
+    let allSuccessful = true
+    newUsers.forEach((e) => {
+      console.log(e.email + ' ' + e.id)
+      const addSingleUserEndpoint = endpoints.ADD_SINGLE_USER_TO_GROUP.replace(
+        ':groupId',
+        groupToAddTo.id
+      )
+
+      console.log(addSingleUserEndpoint)
+      axios
+        .post(addSingleUserEndpoint, {
+          userId: e.id.toString(),
+        })
+        .catch((err) => {
+          console.log(err)
+          allSuccessful = false
+        })
+    })
+  }
+
+  return (
+    <div>
+      <h4>Add/Remove Users to Group</h4>
+      <div className='UsersAndGroups alignItems'>
+        <div>
+          {allUsers.map((item, index) => (
+            <div className='displayRow alignItems' key={index.toString()}>
+              <input
+                type='checkbox'
+                onChange={() => handleUsersOnChange(index)}
+              />
+              <h5>{item.email}</h5>
+            </div>
+          ))}
+        </div>
+        <div>
+          {allGroups.map((item, index) => (
+            <div className='displayRow alignItems' key={index.toString()}>
+              <input
+                type='radio'
+                checked={selectedGroup === index}
+                onChange={() => handleGroupsOnChange(index)}
+              />
+              <h5>{item.name}</h5>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='addRemoveButtonContainer'>
+        <button
+          type='button'
+          className='addRemoveButton'
+          onClick={removeUsersFromGroup}
+        >
+          Remove Users to Group
+        </button>
+        <button
+          type='button'
+          className='addRemoveButton'
+          onClick={assignUsersToGroup}
+        >
+          Assign Users to Group
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default AddUsersToGroup
---
+++ src/components/NewUserForm.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { newUser } from './state/UserState'
+
+import '../component-styles/NewUserForm.css'
+import enpoints from '../enpoints'
+
+const NewUserForm = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const addNewUser = (email, password, confirmPassword) => {
+    // TODO: email validation
+
+    // email not already registered and password == confirm password
+    if (email.length === 0) {
+      setErrorMessage('A new user requires an email')
+    } else if (password.length === 0) {
+      setErrorMessage('Please enter a password')
+    } else if (password === confirmPassword) {
+      console.log('Creating New User')
+      axios
+        .post(enpoints.CREATE_NEW_USER, {
+          email: email,
+          password,
+          confirmPassword,
+        })
+        .then((response) => {
+          alert('user added')
+          setEmail('')
+          setPassword('')
+          setConfirmPassword('')
+          newUser(response.data)
+        })
+        .catch((err) => console.log(err))
+    } else {
+      setErrorMessage('Passwords do not match')
+      setPassword('')
+      setConfirmPassword('')
+    }
+  }
+
+  return (
+    <div className='container'>
+      <h3>Add a New User</h3>
+      <input
+        className='input'
+        type='text'
+        value={email}
+        onChange={(text) => {
+          setErrorMessage('')
+          setEmail(text.target.value)
+        }}
+        placeholder='email'
+      />
+      <input
+        className='input'
+        type='text'
+        value={password}
+        onChange={(text) => {
+          setErrorMessage('')
+          setPassword(text.target.value)
+        }}
+        placeholder='password'
+      />
+      <input
+        className='input'
+        type='text'
+        value={confirmPassword}
+        onChange={(text) => {
+          setErrorMessage('')
+          setConfirmPassword(text.target.value)
+        }}
+        placeholder='confirm password'
+      />
+      <p className='errorMessage'>{errorMessage}</p>
+      <button
+        onClick={() => {
+          console.log('button clicked')
+          addNewUser(email, password, confirmPassword)
+        }}
+      >
+        Add new User
+      </button>
+    </div>
+  )
+}
+
+export default NewUserForm
